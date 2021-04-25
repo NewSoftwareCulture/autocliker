12ffdd42833dfe5896fadb4c0c6fed91bbf02320
Vlados <vlados.pochta@gmail.com>
2021-04-26 01:23:51 +0300

    'init'

---
+++ main.py
@@ -0,0 +1,33 @@
+import pyautogui as auto
+from time import sleep 
+
+class App:
+  x = 200
+  y = 900
+  action = 'right'
+
+  def right_move_mouse(self):
+    self.x += 100
+    self.action = 'right'
+    auto.moveTo(self.x, self.y)
+    auto.mouseDown()
+    auto.mouseUp()
+
+  def left_move_mouse(self):
+    self.x -= 100
+    self.action = 'left'
+    auto.moveTo(self.x, self.y)
+    auto.mouseDown()
+    auto.mouseUp()
+
+  def start(self):
+    while True:
+      if self.action == 'left':
+        self.right_move_mouse()
+      elif self.action == 'right':
+        self.left_move_mouse()
+    
+      sleep(5)
+
+application = App()
+application.start()
